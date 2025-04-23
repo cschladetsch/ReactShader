3501b0d7faace84edc41350fed9784f8d45f8752
christian.schladetsch <christian.schladetsch@gmail.com>
2025-04-24 05:25:00 +1000

    Remove the silly header.

--- math-shaders-demo/src/App.js
+++ math-shaders-demo/src/App.js
@@ -5,9 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Mathematical Shaders</h1>
-      </header>
       <main>
         <MathShaders />
       </main>
